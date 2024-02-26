0769c60cd5d2382dbb1df451ff07e424fe88427f
licq <lee.chaoqun@gmail.com>
2024-02-26 12:35:05 +0800

    use whitelist to instead of blacklist

--- app/src/main/java/com/licq/sportslive/SubCategory.java
+++ app/src/main/java/com/licq/sportslive/SubCategory.java
@@ -4,56 +4,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SubCategory {
-
-    private static final Set<String> blackList;
+    private static final Set<String> whiteList = new HashSet<>();
 
     static {
-        blackList = new HashSet<>();
-        blackList.add("哥斯甲");
-        blackList.add("美联");
-        blackList.add("阿甲");
-        blackList.add("墨联");
-        blackList.add("哥伦甲");
-        blackList.add("日职乙");
-        blackList.add("J联赛");
-        blackList.add("NCAA");
-        blackList.add("澳超");
-        blackList.add("香港超");
-        blackList.add("泰超");
-        blackList.add("土超");
-        blackList.add("乌超");
-        blackList.add("波兰超");
-        blackList.add("苏超");
-        blackList.add("罗甲");
-        blackList.add("德乙");
-        blackList.add("比甲");
-        blackList.add("保超");
-        blackList.add("丹超");
-        blackList.add("乌拉甲");
-        blackList.add("西乙");
-        blackList.add("葡甲");
-        blackList.add("葡超");
-        blackList.add("瑞士超");
-        blackList.add("奧甲");
-        blackList.add("阿联酋超");
-        blackList.add("沙特联");
-        blackList.add("印超");
-        blackList.add("克罗甲");
-        blackList.add("智甲");
-        blackList.add("意乙");
-        blackList.add("荷甲");
-        blackList.add("篮球");
-        blackList.add("男篮非洲杯");
-        blackList.add("男篮世欧预");
-        blackList.add("WCBA");
-        blackList.add("男篮欧国杯");
-        blackList.add("埃及超");
-        blackList.add("希超");
-        blackList.add("秘鲁甲");
-        blackList.add("巴拉甲");
-        blackList.add("男篮亚洲杯");
-        blackList.add("男篮美洲杯");
-        blackList.add("墨西甲");
+        whiteList.add("英超");
+        whiteList.add("西甲");
+        whiteList.add("意甲");
+        whiteList.add("德甲");
+        whiteList.add("法甲");
+        whiteList.add("NBA");
+        whiteList.add("中超");
+        whiteList.add("欧洲杯");
+        whiteList.add("世界杯");
     }
 
     private final String text;
@@ -69,6 +31,6 @@ public class SubCategory {
     }
 
     public boolean wantToWatch() {
-        return !blackList.contains(text);
+        return whiteList.contains(text);
     }
 }
